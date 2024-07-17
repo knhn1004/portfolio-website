33f9602b3b6d6149abed4626a200d8f21147e197
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2024-07-17 16:54:16 -0700

    render computer 3D model

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+"use client";
+
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { useGLTF } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
+
+function Model({ modelPath }: { modelPath: string }) {
+	const { scene } = useGLTF(modelPath);
+	return <primitive object={scene} />;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-		<Button>Click me</Button>
-	</main>
-  );
+	const modelPath = '/computer.glb';
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Canvas
+				gl={{ preserveDrawingBuffer: true }}
+				style={{ width: '400px', height: '400px' }}
+				camera={{ position: [0, 0, 8] }}
+			>
+				<OrbitControls
+					// autoRotate
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+					enableZoom={false}
+					enablePan={false}
+				/>
+				<ambientLight intensity={1} />
+				<directionalLight position={[0, 0, 8]} />
+				<Suspense fallback={null}>
+					<Model modelPath={modelPath} />
+				</Suspense>
+			</Canvas>
+		</main>
+	);
 }
