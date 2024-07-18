aa9f2350e56ef64270e5ec531795e8fc2500a953
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2024-07-18 15:21:56 -0700

    chore: Update UI components and navigation items

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,33 +12,35 @@ const mainNavItems = [
 
 export default function MainNav() {
 	return (
-		<div className="mr-4 hidden gap-2 md:flex items-center justify-between w-full">
-			<div className="flex gap-2">
-				{mainNavItems.map(({ title, link }, index) => (
-					<Button key={index} variant="link" asChild>
-						<Link href={link}>{title}</Link>
+		<div className="container md:px-10">
+			<div className="mr-4 hidden gap-2 md:flex items-center justify-between w-full">
+				<div className="flex gap-2">
+					{mainNavItems.map(({ title, link }, index) => (
+						<Button key={index} variant="link" asChild>
+							<Link href={link}>{title}</Link>
+						</Button>
+					))}
+				</div>
+				<div className="flex gap-2 ml-auto">
+					<Button asChild variant="link">
+						<a
+							href="https://github.com/knhn1004"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Github size={20} />
+						</a>
 					</Button>
-				))}
-			</div>
-			<div className="flex gap-2 ml-auto">
-				<Button asChild variant="link">
-					<a
-						href="https://github.com/knhn1004"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Github size={20} />
-					</a>
-				</Button>
-				<Button asChild variant="link">
-					<a
-						href="https://www.linkedin.com/in/chiahongchou/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Linkedin size={20} />
-					</a>
-				</Button>
+					<Button asChild variant="link">
+						<a
+							href="https://www.linkedin.com/in/chiahongchou/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Linkedin size={20} />
+						</a>
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
 import { Menu as MenuIcon, Github, Linkedin } from 'lucide-react';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const mobileItems = [
 	{ title: 'OC', link: '/' },
@@ -15,10 +15,39 @@ const mobileItems = [
 
 export default function MobileNav() {
 	const [open, setOpen] = useState(false);
+	const [targetLink, setTargetLink] = useState<string | null>(null);
+
+	useEffect(() => {
+		if (!open && targetLink) {
+			const targetId = targetLink.split('#')[1];
+			const targetElement = document.getElementById(targetId);
+
+			if (targetElement) {
+				window.scrollTo({
+					top: targetElement.offsetTop,
+					behavior: 'smooth',
+				});
+			}
+
+			// Reset target link after scrolling
+			setTargetLink(null);
+		}
+	}, [open, targetLink]);
+
+	const handleNavigation = (
+		e: React.MouseEvent<HTMLButtonElement>,
+		link: string
+	) => {
+		if (!link.startsWith('/')) {
+			redirect(link);
+		}
+		e.preventDefault();
+		setTargetLink(link);
+		setOpen(false);
+	};
 
 	return (
 		<Sheet open={open} onOpenChange={setOpen}>
-			{/* This button will trigger open the mobile sheet menu */}
 			<SheetTrigger asChild>
 				<Button variant="ghost" size="icon" className="md:hidden">
 					<MenuIcon />
@@ -33,11 +62,9 @@ export default function MobileNav() {
 							key={index}
 							variant="link"
 							className="w-full text-left"
-							onClick={() => {
-								setOpen(false);
-							}}
+							onClick={e => handleNavigation(e, link)}
 						>
-							<Link href={link}>{title}</Link>
+							<a href={link}>{title}</a>
 						</Button>
 					))}
 					<div className="flex gap-2 mt-4">
